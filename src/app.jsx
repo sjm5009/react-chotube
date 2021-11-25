c339f57deea0e90c4d5aab044b4941c99d839dad
SJM <suhjm5009@gmail.com>
2021-11-25 14:02:32 +0900

    chotube first commit_1

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
-import './app.css';
+import { render } from "@testing-library/react";
+import { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list";
 
 function App() {
-  return (
-    <h1>Hello :)!!</h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyD1yl3_ArbqtDlO52Zt_GmOpO1tTp56Hfw",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  });
+
+  return <VideoList videos={videos}></VideoList>;
+  /* return <h1>hello</h1>; */
 }
 
 export default App;
